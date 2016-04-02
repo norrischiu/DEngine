d4bed5947d5991d41abdae56c5809a675e6baa59
norrischiu <sungchiu@usc.edu>
2016-04-02 18:28:50 +0800

    Changed playerASM to show usage of engine event

---
+++ GameCode/MainPlayer/PlayerASM.cpp
@@ -0,0 +1,67 @@
+// PlayerASM.cpp
+
+// Game include
+#include "PlayerASM.h"
+#include "Event\GameEvent.h"
+
+// Engine include
+#include "DEngine\Graphics\Animation\AnimationController.h"
+#include "DEngine\Event\EngineEvent.h"
+#include "DEngine\Event\EventQueue.h"
+#include "DEngine\Memory\Handle.h"
+
+PlayerASM::PlayerASM(DE::AnimationController * animController)
+	: DE::AnimationStateMachine(animController)
+{
+	AddState("IDLE", "idle");
+	SetAsDefaultState("IDLE");
+	AddState("WALK", "walk");
+	AddState("ATTACK", "attack1");
+
+	AddTransistion("IDLE", "WALK", 1, 0.5f);
+	AddTransistion("WALK", "IDLE", 1, 0.5f);
+	AddTransistion("IDLE", "ATTACK", 1, 0.5f);
+	AddTransistion("ATTACK", "IDLE", 1, 1.0f);
+	AddTransistion("WALK", "ATTACK", 1, 0.5f);
+	AddTransistion("ATTACK", "WALK", 1, 0.5f);
+}
+
+void PlayerASM::Update(float deltaTime)
+{
+	DE::AnimationStateMachine::Update(deltaTime);
+
+	while (!DE::EventQueue::GetInstance()->Empty(DE::GAME_EVENT))
+	{
+		DE::Handle hEvt = DE::EventQueue::GetInstance()->Front(DE::GAME_EVENT);
+		HandleEvent(hEvt);
+		DE::EventQueue::GetInstance()->Pop(DE::GAME_EVENT);
+	}
+}
+
+void PlayerASM::HandleEvent(DE::Handle hEvt)
+{
+	DE::Event* pEvt = (DE::Event*) hEvt.Raw();
+	switch (pEvt->m_ID)
+	{
+		case GameEventID::Player_Walk_START_Event:
+			ChangeStateTo("WALK");
+			break;
+		case GameEventID::Player_Walk_END_Event:
+			ChangeStateTo("IDLE");
+			break;	
+		case GameEventID::Player_Attack_1_START_Event:
+			ChangeStateTo("ATTACK");
+			break;
+		case GameEventID::Player_Attack_1_END_Event:
+			ChangeStateTo("IDLE");
+			break;
+		case DE::EngineEventID::Animation_END_Event:
+			if (strcmp(m_pCurrState->m_sName, "ATTACK") == 0)
+			{
+				DE::Handle hEvt(sizeof(Player_Attack_1_END_Event));
+				new (hEvt) Player_Attack_1_END_Event;
+				DE::EventQueue::GetInstance()->Add(hEvt, DE::GAME_EVENT);
+			}
+			break;
+	}
+}
